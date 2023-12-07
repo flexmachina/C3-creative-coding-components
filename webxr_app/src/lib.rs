728a333a9c42cb5925ee9a0724ceede4aa1665cc
Mayur Patel <mayurp@gmail.com>
2023-12-07 01:19:14 +0000

    Move pipeline creation helper to new file.

--- webxr_app/src/lib.rs
+++ webxr_app/src/lib.rs
@@ -182,65 +182,6 @@ pub struct State {
     window_state: WindowState
 }
 
-fn create_render_pipeline(
-    device: &wgpu::Device,
-    layout: &wgpu::PipelineLayout,
-    color_format: wgpu::TextureFormat,
-    depth_format: Option<wgpu::TextureFormat>,
-    vertex_layouts: &[wgpu::VertexBufferLayout],
-    shader: wgpu::ShaderModuleDescriptor,
-    webxr: bool
-) -> wgpu::RenderPipeline {
-    let shader = device.create_shader_module(shader);
-
-    device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-        label: Some(&format!("{:?}", shader)),
-        layout: Some(layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: "vs_main",
-            buffers: vertex_layouts,
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[Some(wgpu::ColorTargetState {
-                format: color_format,
-                blend: Some(wgpu::BlendState::REPLACE),
-                write_mask: wgpu::ColorWrites::ALL,
-            })],
-        }),
-        primitive: wgpu::PrimitiveState {
-            topology: wgpu::PrimitiveTopology::TriangleList,
-            strip_index_format: None,
-            front_face: if webxr {wgpu::FrontFace::Cw} else {wgpu::FrontFace::Ccw},
-            cull_mode: Some(wgpu::Face::Back),
-            // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-            // or Features::POLYGON_MODE_POINT
-            polygon_mode: wgpu::PolygonMode::Fill,
-            // Requires Features::DEPTH_CLIP_CONTROL
-            unclipped_depth: false,
-            // Requires Features::CONSERVATIVE_RASTERIZATION
-            conservative: false,
-        },
-        depth_stencil: depth_format.map(|format| wgpu::DepthStencilState {
-            format,
-            depth_write_enabled: true,
-            depth_compare: wgpu::CompareFunction::Less,
-            stencil: wgpu::StencilState::default(),
-            bias: wgpu::DepthBiasState::default(),
-        }),
-        multisample: wgpu::MultisampleState {
-            count: 1,
-            mask: !0,
-            alpha_to_coverage_enabled: false,
-        },
-        // If the pipeline will be used with a multiview render pass, this
-        // indicates how many array layers the attachments will have.
-        multiview: None,
-    })
-}
-
 async fn create_redner_state(
     device: wgpu::Device,
     queue: wgpu::Queue,
@@ -381,7 +322,7 @@ async fn create_redner_state(
                 shader_utils::load_shader!(&mut shader_composer, "phong.wgsl", webxr)
             ))
         };
-        create_render_pipeline(
+        wgpu_utils::create_render_pipeline(
             &device,
             &render_pipeline_layout,
             color_format,
@@ -404,7 +345,7 @@ async fn create_redner_state(
                 shader_utils::load_shader!(&mut shader_composer, "light.wgsl", webxr)
             ))
         };
-        create_render_pipeline(
+        wgpu_utils::create_render_pipeline(
             &device,
             &layout,
             color_format,
